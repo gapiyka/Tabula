913ae7068655aa2371c2552afce7adaacd08d1d0
gapiyka <50524296+gapiyka@users.noreply.github.com>
2022-07-06 12:41:22 +0300

    main game logic
    >add finish model
    >change collision detection system + velocity of gliding
    >add logic of tables spawn
    >smthing else ;)

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -1,38 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelBuilder : MonoBehaviour
 {
     [SerializeField] private GameObject[] PlatformPrefabs;
     [SerializeField] private PlayerController player;
+    [SerializeField] private GameObject checkPointPrefab;
 
     private const float GeneratePrefabStep = 82.6f;
     private const float StartOffset = 1.2f;
     private const float StepOffset = 20.65f;
     private const float PrefabYPos = 5f;
     private const float PrefabXPos = -3f;
+    private const float DeleteDelay = 30f;
+    private const float CheckpointRange = 288.75f;
 
-    private float PrefabSpawnPos;
+    private float prefabSpawnPos;
+    private float checkpointSpawnPos;
 
     void Start()
     {
-        PrefabSpawnPos = GeneratePrefabStep- StartOffset;
+        prefabSpawnPos = GeneratePrefabStep - StartOffset;
         GenerateNewBlock();
+        GenerateNewCheckPoint();
     }
 
     void Update()
     {
-        float prefabStep = PrefabSpawnPos - GeneratePrefabStep - StepOffset;
-        if (player.transform.position.z >= prefabStep)
-        {
-            GenerateNewBlock();
-        }
+        float prefabStep = prefabSpawnPos - GeneratePrefabStep - StepOffset;
+        if (player.transform.position.z >= prefabStep) GenerateNewBlock();
+        if (player.transform.position.z >= checkpointSpawnPos) GenerateNewCheckPoint();
     }
 
-    private void GenerateNewBlock()
+    void GenerateNewBlock()
     {
-        Instantiate(PlatformPrefabs[Random.Range(0, 5)],
-                new Vector3(PrefabXPos, PrefabYPos, PrefabSpawnPos),
+        GameObject newBlock = Instantiate(PlatformPrefabs[Random.Range(0, 5)],
+                new Vector3(PrefabXPos, PrefabYPos, prefabSpawnPos),
                 Quaternion.identity, transform);
-        PrefabSpawnPos += GeneratePrefabStep;
+        prefabSpawnPos += GeneratePrefabStep;
+        StartCoroutine(DeleteObject(newBlock, DeleteDelay));
+    }
+    void GenerateNewCheckPoint()
+    {
+        checkpointSpawnPos = player.transform.position.z + CheckpointRange;
+        GameObject newCheckPoint = Instantiate(checkPointPrefab,
+                new Vector3(0, -1, checkpointSpawnPos),
+                Quaternion.Euler(0,90,90), transform);
+        StartCoroutine(DeleteObject(newCheckPoint, DeleteDelay * 4));
+    }
+
+    IEnumerator DeleteObject(GameObject block, float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(block);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,24 +6,32 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private CharacterController controller;
     [SerializeField] private AudioSource pickSource;
+    [SerializeField] private Transform tableSpawn;
+    [SerializeField] private Transform tableForwardSpawn;
+    [SerializeField] private GameObject tablePrefab;
 
     private const float defaultVelocity = -0.45f;
     //private const float defaultSpeed = 6f;
     //private const float mltplSpeed = 1.2f;
-    private const float groundCheckerWidth = 0.2f;
-    private const float groundCheckerHeight = 0.001f;
+    private const float defaultAngle = 17.5f;
+    private const float defaultSleepDelay = 0.25f;
+    private const float levelBorder = 5f;
 
+    
+
+    private Stack tableStack = new Stack();
     private float pSpeed = 6f;
     private float pGravity = -10f;
-    private float jumpForce = 0.4f;
     private Vector3 glideVelocity;
     private bool isGrounded = true;
-    private int tableCounter = 0;
-    //private bool isGliding = false;
+    private bool isGliding = false;
+    private bool courutineEnded = true;
 
     void Update()
     {
         CalculateMovements();
+        CalculateGliding();
+        CheckBorders();
     }
 
     void CalculateMovements()
@@ -32,61 +40,88 @@ public class PlayerController : MonoBehaviour
         Vector3 move = new Vector3(0,0,1);
         controller.Move(move * pSpeed * Time.deltaTime);
 
-        //gliding calculating
-        Vector3 groundCheckerBox = new Vector3(groundCheckerWidth, groundCheckerHeight, groundCheckerWidth);
-        isGrounded = Physics.CheckBox(transform.position, groundCheckerBox);
+        //collision+gravity calculating
         glideVelocity.y += pGravity * Time.deltaTime;
         if (isGrounded)
         {
             if (glideVelocity.y < 0) glideVelocity.y = defaultVelocity;
         }
-        if (Input.GetMouseButton(0))
+        controller.Move(glideVelocity * Time.deltaTime);
+        isGrounded = false;
+    }
+
+    void CalculateGliding()
+    {
+        isGliding = (Input.GetMouseButton(0) && tableStack.Count > 0) ? true : false;
+        float xRotationAngle = 0;
+        if (isGliding && courutineEnded)
         {
             if (Input.GetKey(KeyCode.UpArrow))
             {
-                MoveUp();
+                xRotationAngle = -defaultAngle;
             }
             else if (Input.GetKey(KeyCode.DownArrow))
             {
-                MoveDown();
+                xRotationAngle = defaultAngle;
             }
-            else glideVelocity.y = 0f;
+            GameObject lastTable = (GameObject)tableStack.Pop();
+            lastTable.transform.parent = null;
+            lastTable.transform.position = tableForwardSpawn.position + (Vector3.up * (-xRotationAngle / 100));
+            lastTable.transform.rotation = Quaternion.Euler(-90 + xRotationAngle, 0, 0);
+            StartCoroutine(MoveTableForward());
+            StartCoroutine(DeleteTable(lastTable, 5f));
         }
-
-        controller.Move(glideVelocity * Time.deltaTime);
     }
 
-    void MoveUp()
+    void CheckBorders()
     {
-        glideVelocity.y = jumpForce * -pGravity;
-    }
-
-    void MoveDown()
-    {
-        glideVelocity.y = -jumpForce * -pGravity;
+        if (transform.position.y <= -levelBorder ||
+            transform.position.y >= levelBorder) LooseGame();
     }
 
     void PickTable(GameObject table)
     {
-        tableCounter++;
-        StartCoroutine(DeleteTable(table));
+        StartCoroutine(DeleteTable(table, defaultSleepDelay));
         table.GetComponent<Animator>().SetBool("pick", true);
         pickSource.Play();
+        GameObject newTable = InstatiateTable();
+        tableStack.Push(newTable);
     }
 
-    IEnumerator DeleteTable(GameObject table)
+    IEnumerator DeleteTable(GameObject table, float sleepTime)
     {
-        yield return new WaitForSeconds(0.25f);
+        yield return new WaitForSeconds(sleepTime);
         Destroy(table);
+    }
+
+    IEnumerator MoveTableForward()
+    {
+        courutineEnded = false;
+        yield return new WaitForSeconds(defaultSleepDelay);
+        courutineEnded = true;
+    }
+
+    GameObject InstatiateTable()
+    {
+        Vector3 spawnPos = new Vector3(tableSpawn.position.x, tableSpawn.position.y + (tableStack.Count * 0.11f), tableSpawn.position.z);
+        Quaternion angle = Quaternion.Euler(-90, 0, 0);
+        return Instantiate(tablePrefab, spawnPos, angle, tableSpawn);
+    }
+
+    void LooseGame()
+    {
+        Debug.Log("GG, but u lose");
     }
 
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if (hit.gameObject.tag == "Obstacle") Debug.Log("GG");
+        if (hit.gameObject.tag == "Obstacle") LooseGame();
+        if (hit.gameObject.tag == "Ground") isGrounded = true; ;
     }
 
-    private void OnTriggerEnter(Collider collider)
+    void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.tag == "Pickup") PickTable(collider.gameObject);
+        if (collider.gameObject.tag == "Finish") Debug.Log("GG WP");
     }
 }
